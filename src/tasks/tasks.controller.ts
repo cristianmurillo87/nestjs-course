ba9045e0b7daaace2d003f4a653005e5efdcfc1c
Cristian <cristian@MBP-de-Cristian.fritz.box>
2020-06-27 12:28:00 +0200

    Class 26 - Implement REST API endpoints

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
@@ -12,8 +12,27 @@ export class TasksController {
         return this.tasksService.getAllTasks();
     }
 
+    @Get('/:id')
+    getTaskById(@Param('id') id: string): Task {
+        return this.tasksService.gestTaskById(id);
+    }
+
     @Post()
-    createTask(createTaskDto: CreateTaskDto): Task {
+    createTask(@Body() createTaskDto: CreateTaskDto): Task {
         return this.tasksService.createTask(createTaskDto);
+    }
+
+    @Patch('/:id/status')
+    updateTask(
+        @Param('id') id: string,
+        @Body() newStatus: {status: string}
+    ): Task {
+        const status = TaskStatus[newStatus.status];
+        return this.tasksService.updateTask(id, status);
+    }
+
+    @Delete('/:id')
+    deleteTask(@Param('id') id: string) {
+        return this.tasksService.deleteTask(id);
     }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -11,6 +11,11 @@ export class TasksService {
         return this.tasks;
     }
 
+
+    gestTaskById(id: string): Task {
+        return this.findTask(id);
+    }
+
     createTask(createTaskDto: CreateTaskDto) {
         const {title, description} = createTaskDto;
         
@@ -24,5 +29,26 @@ export class TasksService {
         this.tasks.push(task);
         // return the created task as a good practice when creating a new resource
         return task;
+    }
+
+    deleteTask(id: string): Task {
+        const task: Task = this.findTask(id);
+        if (task) {
+            this.tasks = this.tasks.filter(task => task.id !== id);
+            return task; 
+        }
+        return null;
+    }
+
+    updateTask(id: string, status: TaskStatus): Task {
+        const task = this.findTask(id);
+        if (task) {
+            task.status = status;
+        }
+        return task;
+    }
+
+    private findTask(id: string): Task {
+        return this.tasks.find(task => task.id === id);
     }
 }
