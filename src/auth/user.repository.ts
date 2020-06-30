93ebc8e82b46c0c26190b79991c3b70cd66d589b
Cristian <cristian@MBP-de-Cristian.fritz.box>
2020-06-30 21:47:14 +0200

    Class 56 - Implementing hashing paswords

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,16 +1,19 @@
 import { Repository, EntityRepository } from "typeorm";
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
+import * as bcrypt from 'bcrypt';
 import { User } from "./user.entity";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
-import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
     async signUp(authCredentialsDto: AuthCredentialsDto) {
         const {username, password} = authCredentialsDto;
 
+        // generate a salt per user (not application wide)
         const user = new User();
         user.username = username;
-        user.password = password;
+        user.salt = await bcrypt.genSalt();
+        user.password = await this.hashPassword(password, user.salt);
 
         try {
             await user.save();
@@ -22,5 +25,9 @@ export class UserRepository extends Repository<User> {
                 throw new InternalServerErrorException();
             }
         }
+    }
+
+    private async hashPassword(password: string, salt: string) {
+        return bcrypt.hash(password, salt);
     }
 }
