3e7a7e94a35bac8aa60a40faf26942237d0757c7
Cristian <cristian@MBP-de-Cristian.fritz.box>
2020-07-01 21:20:38 +0200

    Class 61 - Use passport jwt to validate user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,22 +2,31 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(UserRepository)
-        private useRepository: UserRepository) {}
+        private useRepository: UserRepository,
+        private jwtService: JwtService
+        ) {}
 
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         return this.useRepository.signUp(authCredentialsDto);
     }
 
-    async signIn(authCredentialsDto: AuthCredentialsDto) {
+    async signIn(authCredentialsDto: AuthCredentialsDto): Promise<{accessToken: string}> {
         const username = await this.useRepository.validateUserPassword(authCredentialsDto);
         
         if(!username) {
             throw new UnauthorizedException('Invalid credentials');
         }
+
+        const payload: JwtPayload = {username};
+        const accessToken = await this.jwtService.sign(payload);
+
+        return {accessToken}
     }
 }
