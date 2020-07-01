e40e94e604b128c1c705a680aa8ac9133cd8f305
Cristian <cristian@MBP-de-Cristian.fritz.box>
2020-07-01 22:06:41 +0200

    Class 66 - Defining relationships between entities

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,5 +1,6 @@
-import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, Unique } from "typeorm";
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, Unique, OneToMany } from "typeorm";
 import * as bcrypt from 'bcrypt';
+import { Task } from "../tasks/task.entity";
 
 @Entity()
 @Unique(['username']) // Columns that should have unique values
@@ -16,6 +17,13 @@ export class User extends BaseEntity {
 
     @Column()
     salt: string;
+
+    // Users can have many task
+    // when eager is true, the array of task belonging to one user
+    // can be retrieved with user.tasks
+    // only one side of the relationship can be eager (in User, eager must be false) 
+    @OneToMany(type => Task, task => task.user, {eager: true})
+    tasks: Task[]
 
     async validatePassword(password: string): Promise<boolean> {
         const hash = await bcrypt.hash(password, this.salt);
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -6,6 +6,8 @@ import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 import { Task } from './task.entity';
 import { TaskStatus } from './task-status.enum';
+import { GetUser } from 'src/auth/get-user.decorator';
+import { User } from 'src/auth/user.entity';
 
 @Controller('tasks')
 @UseGuards(AuthGuard())
@@ -35,8 +37,11 @@ export class TasksController {
 
     @Post()
     @UsePipes(ValidationPipe)
-    createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
-        return this.tasksService.createTask(createTaskDto);
+    createTask(
+        @Body() createTaskDto: CreateTaskDto,
+        @GetUser() user: User
+        ): Promise<Task> {
+        return this.tasksService.createTask(createTaskDto, user);
     }
 
     @Delete('/:id')
